c86f78b1ede6bb5c907935ac3a74462bc2745800
Ajdin <ajdinproho@gmail.com>
2018-06-10 20:50:47 +0200

    I changed this Hacked By Aydo from Herzegovina

--- functions.php
+++ functions.php
@@ -120,6 +120,10 @@ function html5blank_styles()
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+
+    wp_register_style('nasstil', get_template_directory_uri() . '/nas-style.css', array(), '1.0', 'all');
+    wp_enqueue_style('nasstil'); // Enqueue it!
+    
 }
 
 // Register HTML5 Blank Navigation
--- header.php
+++ header.php
@@ -26,16 +26,16 @@
 	<body <?php body_class(); ?>>
 
 		<!-- wrapper -->
-		<div class="wrapper-headera">
+		<div class="okvir">
 
 			<!-- header -->
-			<header class="header clear" role="banner">
+			<header class="header clear nasHeader" role="banner">
 
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/jabuka.jpg" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
