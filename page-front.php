2fe52da7896eb749c51a9620cd6911bbaf08cbd6
Ajdin <ajdinproho@gmail.com>
2018-06-20 20:23:59 +0200

    added posts loop

--- page-front.php
+++ page-front.php
@@ -4,5 +4,17 @@
         <section class="intro-slide">
             <?php echo do_shortcode('[smartslider3 slider=2]'); ?>
          </section>
+         <?php
+            $args= array(
+                'category_name'=> 'Naslovnica',
+                'posts_per_page'=> 3,
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts()){
+                while($query->have_posts()){
+                    $query->the_post();
+                }
+            }
+        ?>
         </main>
 <?php get_footer(); ?>
